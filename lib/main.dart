e83c617194d979f88ef1d24786b7e766af5a159a
cckn <cckn1985@gmail.com>
2020-06-15 17:56:33 +0900

    Appbar Icon Button

--- lib/main.dart
+++ lib/main.dart
@@ -16,136 +16,32 @@ class MyApp extends StatelessWidget {
   }
 }
 
-const appBarColor = Color(0xffFEA002);
-const backgroundColor = Color(0xffFF9002);
-
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: appBarColor,
-        title: Text("BBANTO ID CARD"),
+        title: Text("AppBar Icon Menu"),
         elevation: 0,
         centerTitle: true,
+        leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              print("menu button is clicked");
+            }),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {
+                print("shopping cart button is clicked");
+              }),
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                print("search button is clicked");
+              }),
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30, 50, 30, 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('images/sample.png'),
-                radius: 60,
-              ),
-            ),
-            Divider(
-              height: 60,
-              color: Colors.grey[850],
-              thickness: 0.5,
-            ),
-            Text(
-              "NAME",
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "BBANTO",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              "BBANTO POWER LEVEL",
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "14",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Icon(Icons.check_circle_outline),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "Using lightsaber",
-                      style: TextStyle(
-                        fontSize: 16,
-                        letterSpacing: 1,
-                      ),
-                    )
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    Icon(Icons.check_circle_outline),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "Face hero tattoo",
-                      style: TextStyle(
-                        fontSize: 16,
-                        letterSpacing: 1,
-                      ),
-                    )
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    Icon(Icons.check_circle_outline),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "Fire flames",
-                      style: TextStyle(
-                        fontSize: 16,
-                        letterSpacing: 1,
-                      ),
-                    )
-                  ],
-                ),
-                Center(
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage('images/sample.png'),
-                    radius: 40,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-      backgroundColor: backgroundColor,
     );
   }
 }
