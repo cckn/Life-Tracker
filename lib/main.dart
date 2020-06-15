52f6424df1b8107051ed3daec81aadbef1779b51
cckn <cckn1985@gmail.com>
2020-06-15 17:13:15 +0900

    sample UI clone cording

--- lib/main.dart
+++ lib/main.dart
@@ -6,40 +6,146 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: "Life Tracker",
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: """CCKN's Life Tracker"""),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+const appBarColor = Color(0xffFEA002);
+const backgroundColor = Color(0xffFF9002);
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        backgroundColor: appBarColor,
+        title: Text("BBANTO ID CARD"),
+        elevation: 0,
+        centerTitle: true,
       ),
-      body: Text("Hello LifeTracker!"),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 50, 30, 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('images/sample.png'),
+                radius: 60,
+              ),
+            ),
+            Divider(
+              height: 60,
+              color: Colors.grey[850],
+              thickness: 0.5,
+            ),
+            Text(
+              "NAME",
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "BBANTO",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "BBANTO POWER LEVEL",
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "14",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.check_circle_outline),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Using lightsaber",
+                      style: TextStyle(
+                        fontSize: 16,
+                        letterSpacing: 1,
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.check_circle_outline),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Face hero tattoo",
+                      style: TextStyle(
+                        fontSize: 16,
+                        letterSpacing: 1,
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.check_circle_outline),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Fire flames",
+                      style: TextStyle(
+                        fontSize: 16,
+                        letterSpacing: 1,
+                      ),
+                    )
+                  ],
+                ),
+                Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('images/sample.png'),
+                    radius: 40,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: backgroundColor,
     );
   }
 }
