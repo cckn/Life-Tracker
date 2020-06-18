be84eaf29f8f818ebcd1ba4654482cb4f971f367
cckn <cckn1985@gmail.com>
2020-06-18 15:08:09 +0900

    Login UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 void main() => runApp(MyApp());
 
@@ -33,7 +34,70 @@ class Login extends StatelessWidget {
         ],
       ),
       drawer: Drawer(),
-      body: Center(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(
+                top: 50,
+              ),
+            ),
+            Center(
+              child: Image(
+                image: AssetImage('images/chef.gif'),
+                width: 170,
+                height: 190,
+              ),
+            ),
+            Form(
+              child: Theme(
+                data: ThemeData(
+                  primaryColor: Colors.teal,
+                  inputDecorationTheme: InputDecorationTheme(
+                    labelStyle: TextStyle(color: Colors.teal, fontSize: 15),
+                  ),
+                ),
+                child: Container(
+                  padding: EdgeInsets.all(40),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Enter "Dice"',
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Enter Password',
+                        ),
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                      ButtonTheme(
+                        child: RaisedButton(
+                          onPressed: () {},
+                          child: Icon(
+                            Icons.arrow_forward,
+                            color: Colors.white,
+                            size: 35,
+                          ),
+                        ),
+                        minWidth: 100,
+                        height: 50,
+                        buttonColor: Colors.orangeAccent,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
