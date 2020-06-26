8db2856ad78789bdee37924bc941b8e1cc92ab20
cckn <cckn1985@gmail.com>
2020-06-26 18:08:21 +0900

    auto_size_text: ^2.1.0 적용

--- lib/screens/home/record_tile.dart
+++ lib/screens/home/record_tile.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart' as intl;
 import 'package:lifetracker/data/app_state.dart';
@@ -21,32 +22,36 @@ class RecordTile extends StatelessWidget {
       padding: const EdgeInsets.only(left: 10, right: 10),
       child: Column(
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: Text(
-                  intl.DateFormat('a hh:mm').format(record.time),
-                  style: TextStyle(fontSize: 10, color: Colors.grey),
+          SizedBox(
+//            height: 50,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Text(
+                    intl.DateFormat('a hh:mm').format(record.time),
+                    style: TextStyle(fontSize: 10, color: Colors.grey),
+                  ),
                 ),
-              ),
-              Expanded(
-                flex: 8,
-                child: Text(
-                  record.text,
-                  textAlign: TextAlign.left,
+                Expanded(
+                  flex: 8,
+                  child: AutoSizeText(
+                    record.text,
+                    style: TextStyle(fontSize: 20.0),
+                    maxLines: 2,
+                  ),
                 ),
-              ),
-              Expanded(
-                flex: 1,
-                child: IconButton(
-                  icon: Icon(Icons.delete),
-                  onPressed: () {
-                    _buttonClickHandler();
-                  },
-                ),
-              )
-            ],
+                Expanded(
+                  flex: 1,
+                  child: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      _buttonClickHandler();
+                    },
+                  ),
+                )
+              ],
+            ),
           ),
           Divider(
             color: Colors.grey,
